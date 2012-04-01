0212e6e675f0f5f1dcd8bf5ec02f511d3a22c4c0
Alexis Desktop <amejias101@qc.cuny.edu>
2012-03-31 22:34:34 -0400

    * added the gridpak folder

---
+++ FOOTER.php
@@ -0,0 +1,9 @@
+<!--SPACER-->
+	<div class="container_48" id="spacer4">
+    	<div class="grid_44 push_2 spacer4"></div>
+    </div>
+<!--SPACER-->    
+    
+<?php echo $main->main_footer_WHOLE(); ?>
+</body>
+</html>
